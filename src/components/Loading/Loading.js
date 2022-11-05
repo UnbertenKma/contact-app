1235979dcdbbdf0eacdd8725e0cf76f716f48d6d
UnbertenKma <mvthang01@gmail.com>
2022-11-05 23:09:36 +0700

    make it better

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,18 +1,21 @@
+import React from "react";
 import "./loading.css";
 
-function Loading(props) {
+const Loading = () => {
   return (
-    <div class="middle">
-      <div class="bar bar1"></div>
-      <div class="bar bar2"></div>
-      <div class="bar bar3"></div>
-      <div class="bar bar4"></div>
-      <div class="bar bar5"></div>
-      <div class="bar bar6"></div>
-      <div class="bar bar7"></div>
-      <div class="bar bar8"></div>
+    <div>
+      <div class="middle">
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
+        <div class="bar bar4"></div>
+        <div class="bar bar5"></div>
+        <div class="bar bar6"></div>
+        <div class="bar bar7"></div>
+        <div class="bar bar8"></div>
+      </div>
     </div>
   );
-}
+};
 
 export default Loading;
